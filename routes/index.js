a4e30859fe7214f14dda3a29f712b06256fea9c8
Ramesh <48806004+abrameshjs@users.noreply.github.com>
2019-05-17 19:51:35 +0200

    V1-Go-Live

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,6 @@ var router = express.Router();
 var folders = [];
 var thumbnail = '';
 
-
 fs.readdir(testFolder, (err, files) => {
   files.forEach((file, i) => {
     var obj = {};
@@ -17,7 +16,7 @@ fs.readdir(testFolder, (err, files) => {
     } else if (file.toLowerCase().indexOf('video')!=-1) {
       thumbnail = 'fas fa-video';
     } else if (file.toLowerCase().indexOf('audio')!=-1) {
-      thumbnail = 'fa fa-music';
+      thumbnail = 'fa fa-headphones';
     } else {
       thumbnail = 'fa fa-folder';
     }
@@ -29,9 +28,7 @@ fs.readdir(testFolder, (err, files) => {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {  
-  res.render('index', { title: 'E-Library', folders });  
+  res.render('index', { title: 'நூலகம்', folders });  
 });
-
-
 
 module.exports = router;
